b8dfeedde5364cdaca3c987d82bf8c490b4d5dbd
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-12-09 02:08:42 +0500

    add shop page additionally

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <h1 className='text-4xl text-[#000]'>
+        This Shop Page is Not in Figma Template 8.
+      </h1>
+    </div>
+  )
+}
+
+export default Shop
