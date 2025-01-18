473669ebdbb88e106f8df9b1af7dafca462e5555
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-18 17:53:51 +0500

    remove eslinit error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import Company5 from '../../public/company5.png'
 import Company6 from '../../public/company6.png'
 import Company7 from '../../public/company7.png'
 import SectionHeading from "@/components/SectionHeading";
-import ProductCard from "@/components/ProductCard";
+// import ProductCard from "@/components/ProductCard";
 
 import Prod2 from '../../public/product2.png';
 import Prod3 from '../../public/product3.png';
@@ -89,19 +89,19 @@ export default function Home() {
       <section className="mt-32 px-8">
         <SectionHeading heading="Featured Products" />
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-12 ">
-            <ProductCard id={1} sales={true} badge={true} InfoSection={true} productCounts={false} imagePath={`${Prod15.src}`     } />
-            <ProductCard id={1} sales={false} badge={true} InfoSection={true} productCounts={false} imagePath={`${Prod2.src}`}/>
-            <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={`${Prod3.src}`}/>
-            <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={`${Prod4.src}`}/>
+            {/* <ProductCard id={1} sales={true} badge={true} InfoSection={true} productCounts={false} imagePath={`${Prod15.src}`     } /> */}
+            {/* <ProductCard id={1} sales={false} badge={true} InfoSection={true} productCounts={false} imagePath={`${Prod2.src}`}/> */}
+            {/* <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={`${Prod3.src}`}/> */}
+            {/* <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={`${Prod4.src}`}/> */}
         </div>
       </section>
       {/* Top Categories  */}
       <section className="mt-32 px-8">
         <SectionHeading heading="Top categories" />
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 lg:gap-10 mt-12 ">
-            <ProductCard id={1} badge={false} InfoSection={false} productCounts={true} imagePath={`${Cat2.src}`}/>
-            <ProductCard id={1} badge={false} InfoSection={false} productCounts={true} imagePath={`${Cat3.src}`}/>
-            <ProductCard id={1} badge={false}  InfoSection={false} productCounts={true} imagePath={`${Cat1.src}`}/>
+            {/* <ProductCard id={1} badge={false} InfoSection={false} productCounts={true} imagePath={`${Cat2.src}`}/> */}
+            {/* <ProductCard id={1} badge={false} InfoSection={false} productCounts={true} imagePath={`${Cat3.src}`}/> */}
+            {/* <ProductCard id={1} badge={false}  InfoSection={false} productCounts={true} imagePath={`${Cat1.src}`}/> */}
         </div>
       </section>
 
@@ -117,14 +117,14 @@ export default function Home() {
           <SectionHeading heading="Products" />
         </div>
         <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 ">
-          <ProductCard id={1} sales={true} badge={true} InfoSection={true} productCounts={false} imagePath={Prod15.src} />
-            <ProductCard id={1} sales={false} badge={true} InfoSection={true} productCounts={false} imagePath={Prod2.src} />
-            <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={Prod3.src} />
-            <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={Prod4.src} />
-            <ProductCard id={1} sales={true} badge={true}  InfoSection={true} productCounts={false} imagePath={Prod15.src} />
-            <ProductCard id={1} sales={false} badge={true} InfoSection={true} productCounts={false} imagePath={Prod6.src} />
-            <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={Prod7.src} />
-            <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={Prod8.src} />
+          {/* <ProductCard id={1} sales={true} badge={true} InfoSection={true} productCounts={false} imagePath={Prod15.src} /> */}
+            {/* <ProductCard id={1} sales={false} badge={true} InfoSection={true} productCounts={false} imagePath={Prod2.src} /> */}
+            {/* <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={Prod3.src} /> */}
+            {/* <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={Prod4.src} /> */}
+            {/* <ProductCard id={1} sales={true} badge={true}  InfoSection={true} productCounts={false} imagePath={Prod15.src} /> */}
+            {/* <ProductCard id={1} sales={false} badge={true} InfoSection={true} productCounts={false} imagePath={Prod6.src} /> */}
+            {/* <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={Prod7.src} /> */}
+            {/* <ProductCard id={1} badge={false} InfoSection={true} productCounts={false} imagePath={Prod8.src} /> */}
         </div>
       </section>
     </div>
