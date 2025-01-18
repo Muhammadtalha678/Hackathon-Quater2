cc88ab1d1b874b55cb010eda4b51cc70db666074
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-18 17:50:04 +0500

    update dynamic api of product

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,16 +1,26 @@
 import { client } from "@/sanity/lib/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params
+export async function GET(req: NextRequest) {
+
     try {
+        const url = new URL(req.url)
+        console.log(url);
+        const id = url.pathname.split("/").pop();
+        console.log("id", id);
+
+        if (!id) {
+            return NextResponse.json({
+                error: true, data: null, message: "Product id is Missing"
+            })
+        }
         const response = await client.fetch(`*[_type == "products" && _id == $id][0]`, { id }) //[0] return single document not array
         if (response) {
             return NextResponse.json({ error: false, data: response, message: "Product fetch successfully" })
 
         }
         else {
-            return NextResponse.json({ error: false, data: null, message: "Product not found" })
+            return NextResponse.json({ error: true, data: null, message: "Product not found" })
 
         }
     } catch (error) {
