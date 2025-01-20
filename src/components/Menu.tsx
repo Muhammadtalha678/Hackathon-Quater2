961196592efe2ba8ce79619fc9b1b2baa25454c2
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-21 01:16:50 +0500

    update hamburger

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -15,7 +15,7 @@ const Menu = ({size}:{size:number}) => {
           <RxHamburgerMenu size={size} className='cursor-pointer ' onClick={handleHamburger} />
     {
             open && 
-            <div className='absolute w-full bg-[#F0F2F3] text-white left-0  top-48 h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-xl z-10'>
+            <div className='absolute w-full bg-[#F0F2F3] text-white left-0  top-60 h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-xl z-10'>
                {navLinks.map((link,index) => {
                       return <NavLink name={link.name} path={link.path} key={index+1} onclick={() => {setTimeout(() => {
                         setOpen(!open)
