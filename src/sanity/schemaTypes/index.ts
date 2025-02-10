9f6770a82519197f8fae9e6c38c91a4d39c54ac4
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-10 09:53:36 +0500

    add user schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import products from './products'
 import category from './category'
-import abc from './abc'
+import user from './user'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [products, category, abc],
+  types: [products, category, user],
 }
